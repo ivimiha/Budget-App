05d6a2e18896395d47762e7b7b2c01be403c5604
Ivica <ivimiha@gmail.com>
2019-08-19 22:08:58 +0200

    Implemented Mode Patter > Setup first event listeners, Read input data

---
+++ app.js
@@ -0,0 +1,81 @@
+// BUDGET CONTROLLER
+
+var budgetController = (function(){
+
+
+})();
+
+
+// UI CONTROLLER
+
+var UIController = (function(){
+    
+    var DOMstrings = {                                              //To keep all the DOM queries in one place so it is easier to manage them later on.
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,               // Get the entry type value. It can be either inc(+) or exp(-)
+                description: document.querySelector(DOMstrings.inputDescription).value,         // Get the description entry. 
+                value: document.querySelector(DOMstrings.inputValue).value                      // Get the actual amount of money
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function (){                             //Function that captures and handles the input
+        //1. Get the input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+
+        //2. Add the item to the budget controller
+
+        //3. Add the new item to the user interface
+
+        //4. Calc the budget
+
+        //5. Display the budget
+        
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem); //
+
+    document.addEventListener("keypress", function(event){   //Add the result when Enter key is pressed also
+        if (event.keyCode === 13 || event.which === 13) {    // Use both to be more precise. .which is for ASCII code. 
+            ctrlAddItem();
+        }
+
+    })
+
+})(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
