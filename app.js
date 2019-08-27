5dc977d58a971e6d9f14476430341a449aaf036f
Ivica <ivimiha@gmail.com>
2019-08-27 22:12:41 +0200

    Show budget updates on the UI. Calculate budget perecentage and show it on the UI.

--- app.js
+++ app.js
@@ -105,7 +105,11 @@ var UIController = (function () {
         inputValue: ".add__value",
         inputBtn: ".add__btn",
         incomeContainer: ".income__list",
-        expensesContainer: ".expenses__list"
+        expensesContainer: ".expenses__list",
+        budgetLable: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expensesLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
     }
 
     return {
@@ -157,6 +161,19 @@ var UIController = (function () {
                 
             },
 
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLable).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = "----"
+            }
+
+        },    
+
         getDOMstrings: function() {
         return DOMstrings;
         }
@@ -189,7 +206,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         //3. Display the budget
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
 
     };
@@ -220,6 +237,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log("Application has started");
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
