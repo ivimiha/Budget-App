4bb36e504f84c17eec960b58a0bfea0cd25317e6
Ivica <ivimiha@gmail.com>
2019-08-30 20:01:07 +0200

    Some UI changes about the input fields.

--- app.js
+++ app.js
@@ -171,6 +171,13 @@ var UIController = (function () {
         return (type === "exp" ? "-" : "+") + " " + int + "." + dec;
     };
 
+    var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i)
+        }
+
+    };
+
     return {
         getInput: function () {
             return {
@@ -248,13 +255,6 @@ var UIController = (function () {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function (list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i)
-                }
-
-            };
-
             nodeListForEach(fields, function (current, index) {
                 if (percentages[index] > 0) {
                     current.textContent = percentages[index] + "%";
@@ -272,6 +272,16 @@ var UIController = (function () {
             month = now.getMonth();
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
+        },
+
+        changedType: function(){
+            var fields = document.querySelectorAll(DOMstrings.inputType + "," + DOMstrings.inputDescription + "," + DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle("red-focus")
+            });
+            document.querySelector(DOMstrings.inputBtn).classList.toggle("red")
+
         },
 
         getDOMstrings: function () {
@@ -297,6 +307,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener("change", UICtrl.changedType);
     };
 
 
